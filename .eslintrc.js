0c2160e2a7b2a4b340ca8f6c767e78bbbb467bb6
root <root@vicibox12.suse>
2026-03-18 19:10:32 +0100

    feat: set up ESLint and Prettier configurations

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
+}
